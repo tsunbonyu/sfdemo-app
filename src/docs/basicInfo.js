9500ad7524651268ead678ca00715c9845c26e40
d0y00h5 <david.yu@walmartlabs.com>
2023-05-23 16:46:13 -0700

    Update title

--- src/docs/basicInfo.js
+++ src/docs/basicInfo.js
@@ -2,12 +2,12 @@ module.exports = {
     openapi:"3.0.1",
     info:{
         version:"1.0.0",
-        title:"Orders",
+        title:"Mega Buy Engine Demo App",
         description: "Orders API",
-        contact:{
-            name:"Kennedy Mwangi",
-            email:"info@mkibui.me",
-            url:"https://mkibui.me"
-        }
+        // contact:{
+        //     name:"Kennedy Mwangi",
+        //     email:"info@mkibui.me",
+        //     url:"https://mkibui.me"
+        // }
     }
 }
